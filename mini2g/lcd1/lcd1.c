e873dd690b8941b5597d43e4c24b019771c07379
dwelch <dwelch@dwelch.com>
2013-06-20 16:20:54 -0400

    lcd1 printing some processor id stuff

--- mini2g/lcd1/lcd1.c
+++ mini2g/lcd1/lcd1.c
@@ -11,6 +11,21 @@ int main ( void )
     init_lcd();
     hexstring(0x12345678);
     hexstring(0xABCDEF90);
+    //hexstring(GET32(0x70000000)); //pp5022
+    if(((GET32(0x70000000)>>24)&0xFF)==0x32)
+    {
+        //pp5022
+        hexstring(GET32(0x60000000)); //pp5022
+        hexstring(GET32(0x4001ff18)); //pp5022
+        hexstring(GET32(0x4001ff1c)); //pp5022
+    }
+    else
+    {
+        //pp5002
+        hexstring(GET32(0xc4000000)); //pp5002
+        hexstring(GET32(0x40017f18)); //pp5002
+        hexstring(GET32(0x40017f1c)); //pp5002
+    }
     return(0);
 }
 
